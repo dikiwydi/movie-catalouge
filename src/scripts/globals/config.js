bbe834d8a9a60a170d04b0b9bed919cadc47958f
dikiwydi <dikiwahyudi268@gmail.com>
2023-10-19 08:27:16 +0700

    add tampilan api

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+  KEY: 'e194fe3efd39620a5242ac48881d9fae',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us',
+};
+
+export default CONFIG;
